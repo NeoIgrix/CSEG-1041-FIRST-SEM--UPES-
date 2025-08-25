07e1c9401ef969efad6a43ce28fea49788ab69d8
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-08-25 18:19:24 +0530

    Update Q6.c

--- #100DAYSOFCODE/Q6.c
+++ #100DAYSOFCODE/Q6.c
@@ -1,14 +1,18 @@
 //Q6:Write a program to swap two numbers using a third variable.
 
-#include <stdio.h>
-int main(){
+    #include <stdio.h>
+    int main(){
+    
     int a,b,c;
-     scanf("%d %d" ,&a,&b);
+    scanf("%d %d" ,&a,&b);
+    
     c=a;
     a=b;
     b=c;
-   printf("%d\n", a);   
+    
+    printf("%d\n", a);   
     printf("%d", b);
     return 0;
 
-}
+    }
+
