cde08a1dd7ab4810a96bc0aaf6150dfd1d647125
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-08-30 13:24:37 +0530

    Update Q17.c

--- #100DAYSOFCODE/Q17.c
+++ #100DAYSOFCODE/Q17.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 int main() {
-    float a, b, c, discriminant, root1, root2, realPart, imagPart;
+    float a, b, c, d, r1, r2, real, imaginary;
 
     printf("Enter coefficients a, b and c: ");
     scanf("%f %f %f", &a, &b, &c);
@@ -14,30 +14,31 @@ int main() {
         return 0;
     }
 
-    discriminant = b*b - 4*a*c;
+     d = b*b - 4*a*c;
 
-    if (discriminant > 0) {
+    if (d > 0) {
         
-        root1 = (-b + sqrt(discriminant)) / (2*a);
-        root2 = (-b - sqrt(discriminant)) / (2*a);
+        r1 = (-b + sqrt(d)) / (2*a);
+        r2 = (-b - sqrt(d)) / (2*a);
         printf("Roots are real and distinct:\n");
-        printf("Root1 = %.2f, Root2 = %.2f\n", root1, root2);
+        printf("Root1 = %f, Root2 = %f\n", r1, r2);
     }
-    else if (discriminant == 0) {
+    else if (d == 0) {
         
-        root1 = root2 = -b / (2*a);
+        r1 = r2 = -b / (2*a);
         printf("Roots are real and equal:\n");
-        printf("Root1 = Root2 = %.2f\n", root1);
+        printf("Root1 = Root2 = %f\n", r1);
     }
     else {
         
-        realPart = -b / (2*a);
-        imagPart = sqrt(-discriminant) / (2*a);
-        printf("Roots are imaginary (complex conjugates):\n");
-        printf("Root1 = %.2f + %.2fi\n", realPart, imagPart);
-        printf("Root2 = %.2f - %.2fi\n", realPart, imagPart);
+        real = -b / (2*a);
+        imaginary = sqrt(-d) / (2*a);
+        printf("Roots are imaginary \n");
+        printf("Root1 = %f + %f\n", real, imaginary);
+        printf("Root2 = %f - %f\n", real, imaginary);
     }
 
     return 0;
 }
+
 
