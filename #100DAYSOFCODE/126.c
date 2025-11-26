1e158a671eeb645c053e6d6b3c06bd8fd57e762c
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-11-26 09:03:53 +0530

    Format code and clean up comments in 126.c

--- #100DAYSOFCODE/126.c
+++ #100DAYSOFCODE/126.c
@@ -7,24 +7,25 @@ int main() {
     char filename[100];
     int ch;
 
-    // Get the filename from the user
+    
     printf("Enter the filename to read: ");
     scanf("%s", filename);
 
-    // Try to open the file in read mode
+   
     file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    // Read and display the content of the file
+   
     printf("File content:\n");
     while ((ch = fgetc(file)) != EOF) {
         putchar(ch);
     }
 
-    // Close the file
+   
     fclose(file);
     return 0;
+
 }
