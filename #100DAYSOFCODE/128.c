4b41ba00bfdd5afb33d5df055e854fbd2fa4ca19
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-11-26 09:02:41 +0530

    Create 128.c

--- #100DAYSOFCODE/128.c
+++ #100DAYSOFCODE/128.c
@@ -9,18 +9,18 @@ int main() {
     int ch;
     int vowels = 0, consonants = 0;
 
-    // Get the filename from the user
+   
     printf("Enter the filename to read: ");
     scanf("%s", filename);
 
-    // Try to open the file in read mode
+   
     file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    // Read characters from the file and count vowels and consonants
+    
     while ((ch = fgetc(file)) != EOF) {
         ch = tolower(ch);
         if (isalpha(ch)) {
@@ -32,12 +32,13 @@ int main() {
         }
     }
 
-    // Close the file
+    
     fclose(file);
 
-    // Display the counts
+    
     printf("Vowels: %d\n", vowels);
     printf("Consonants: %d\n", consonants);
 
     return 0;
+
 }
