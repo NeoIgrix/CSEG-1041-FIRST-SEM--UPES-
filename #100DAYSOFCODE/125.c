0e32e410a6444186fee22e241a62567552cbaec1
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-11-26 09:04:34 +0530

    Update 125.c

--- #100DAYSOFCODE/125.c
+++ #100DAYSOFCODE/125.c
@@ -7,28 +7,29 @@ int main() {
     char filename[100];
     char new_line[256];
 
-    // Get the filename from the user
+   
     printf("Enter the filename to append to: ");
     scanf("%s", filename);
 
-    // Open the file in append mode
+   
     file = fopen(filename, "a");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    // Get the new line of text from the user
+   
     printf("Enter a new line of text: ");
     getchar();  // Consume the newline character left by scanf
     fgets(new_line, sizeof(new_line), stdin);
 
-    // Append the new line to the file
+    
     fputs(new_line, file);
 
-    // Close the file
+    
     fclose(file);
 
     printf("Text appended successfully.\n");
     return 0;
+
 }
