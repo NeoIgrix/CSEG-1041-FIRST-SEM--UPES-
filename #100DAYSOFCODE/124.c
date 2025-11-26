cbd7031401541a96b8fa2bd8bfcd9f6b0d851ab1
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-11-26 09:05:01 +0530

    Update 124.c

--- #100DAYSOFCODE/124.c
+++ #100DAYSOFCODE/124.c
@@ -7,20 +7,20 @@ int main() {
     char source_filename[100], dest_filename[100];
     int ch;
 
-    // Get the source and destination filenames from the user
+   
     printf("Enter source file name: ");
     scanf("%s", source_filename);
     printf("Enter destination file name: ");
     scanf("%s", dest_filename);
 
-    // Open the source file for reading
+   
     source = fopen(source_filename, "r");
     if (source == NULL) {
         perror("Error opening source file");
         return 1;
     }
 
-    // Open the destination file for writing
+    
     dest = fopen(dest_filename, "w");
     if (dest == NULL) {
         perror("Error opening destination file");
@@ -28,15 +28,16 @@ int main() {
         return 1;
     }
 
-    // Copy content from source to destination
+   
     while ((ch = fgetc(source)) != EOF) {
         fputc(ch, dest);
     }
 
-    // Close the files
+    
     fclose(source);
     fclose(dest);
 
     printf("File copied successfully.\n");
     return 0;
+
 }
