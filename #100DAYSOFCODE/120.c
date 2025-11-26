f3b6350f64d9b9f0395716c6bed6867e72668f6e
NeoIgrix <dakshchaudhary48144@gmail.com>
2025-11-26 09:06:32 +0530

    Update 120.c

--- #100DAYSOFCODE/120.c
+++ #100DAYSOFCODE/120.c
@@ -5,11 +5,11 @@
 
 void toSentenceCase(char str[]) {
     int i = 0;
-    // Convert first character to uppercase
+   
     str[i] = toupper(str[i]);
     i++;
 
-    // Convert remaining characters
+   
     while (str[i] != '\0') {
         if (str[i - 1] == '.') {
             str[i] = toupper(str[i]);
@@ -21,7 +21,7 @@ void toSentenceCase(char str[]) {
 int main() {
     char str[100];
 
-    // Input string
+   
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
@@ -30,3 +30,4 @@ int main() {
 
     return 0;
 }
+
